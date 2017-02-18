f59be2d3476a3e8a1e79bc56ccdba8f7dcea2162
Andrei Spataru <spataruac@gmail.com>
2017-02-18 01:35:20 +0100

    added mvn release plugin, developer connection to github and fixed test output

--- src/test/java/TestIntegrationBatch.java
+++ src/test/java/TestIntegrationBatch.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 /**
  * Created by aspataru on 2/11/17.
  */
-public class TestBatch {
+public class TestIntegrationBatch {
 
     private final String fullPath1 = getClass().getResource("inputFile1").getPath();
     private final String fullPath2 = getClass().getResource("inputFile2").getPath();
@@ -14,8 +14,8 @@ public class TestBatch {
 
     @Test
     public void runFullBatch() {
-        new Batch(generator, fullPath1, "out1.txt").run();
-        new Batch(generator, fullPath2, "out2.txt").run();
+        new Batch(generator, fullPath1, "target/out1.txt").run();
+        new Batch(generator, fullPath2, "target/out2.txt").run();
     }
 
 }
