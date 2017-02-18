277d3bff98a1b01299aae2111fe83ebfa1959f5e
Andrei Spataru <spataruac@gmail.com>
2017-02-18 01:53:59 +0100

    completed integration test

--- src/test/java/TestIntegrationBatch.java
+++ src/test/java/TestIntegrationBatch.java
@@ -3,19 +3,31 @@ import generator.Generator;
 import generator.IndexBoundedGenerator;
 import org.junit.Test;
 
+import java.io.File;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 /**
  * Created by aspataru on 2/11/17.
  */
 public class TestIntegrationBatch {
 
-    private final String fullPath1 = getClass().getResource("inputFile1").getPath();
-    private final String fullPath2 = getClass().getResource("inputFile2").getPath();
+    private final String inPath1 = getClass().getResource("input/inputFile1").getPath();
+    private final String inPath2 = getClass().getResource("input/inputFile2").getPath();
+    private final String validationPath1 = getClass().getResource("output/out1").getPath();
+    private final String validationPath2 = getClass().getResource("output/out2").getPath();
+    private final String outPath1 = "target/out1.txt";
+    private final String outPath2 = "target/out2.txt";
     private final Generator generator = new IndexBoundedGenerator();
+
 
     @Test
     public void runFullBatch() {
-        new Batch(generator, fullPath1, "target/out1.txt").run();
-        new Batch(generator, fullPath2, "target/out2.txt").run();
+        new Batch(generator, inPath1, outPath1).run();
+        assertThat(new File(outPath1)).hasSameContentAs(new File(validationPath1));
+
+        new Batch(generator, inPath2, "target/out2.txt").run();
+        assertThat(new File(outPath2)).hasSameContentAs(new File(validationPath2));
     }
 
 }
